dd81ee98d697fb35c405f3f788e0715ca9a7fe48
Jonatas <Jonatas@JONATAS-PC>
2016-11-14 19:01:55 -0200

    Add project files.

---
+++ AppXamarin/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppXamarin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            calcularICMS(1000);
+        }
+
+
+        public static decimal calcularICMS(decimal ValorMercadoria)
+        {
+            return (ValorMercadoria * (18m / 100m));
+        }
+    }
+}
---
+++ TesteAppXamarin/TesteAppXamarin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppXamarin;
+
+namespace TesteAppXamarin
+{
+    [TestClass]
+    public class TesteLifeCycle
+    {
+        [TestMethod]
+        public void Teste_CalculoICMS()
+        {
+            decimal resultado = Program.calcularICMS(1000);
+            Assert.AreEqual(180, resultado);
+        }
+    }
+}
