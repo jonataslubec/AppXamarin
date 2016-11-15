a8f62b31d9c7bcac3b239b58821e84a330d30584
Jonatas Lubec <jonataslbc@gmail.com>
2016-11-15 09:58:19 -0200

    Update TesteAppXamarin.cs

--- TesteAppXamarin/TesteAppXamarin.cs
+++ TesteAppXamarin/TesteAppXamarin.cs
@@ -10,21 +10,21 @@ namespace TesteAppXamarin
         [TestMethod]
         public void Teste_Soma()
         {
-            int resultado = ProjetoCalculadora.Calculadora.soma(4, 4);
+            int resultado = AppXamarin.Program.soma(4, 4);
             Assert.AreEqual(8, resultado);
         }
 
         [TestMethod]
         public void Teste_CalculoICMS_NF01()
         {
-            decimal resultado = ProjetoCalculadora.Calculadora.calculoICMS_NF01(10000);
+            decimal resultado = AppXamarin.Program.calculoICMS_NF01(10000);
             Assert.AreEqual(1800, resultado);
         }
 
         [TestMethod]
         public void Teste_CalculoICMS_NF02()
         {
-            decimal resultado = ProjetoCalculadora.Calculadora.calculoICMS_NF02(10000);
+            decimal resultado = AppXamarin.Program.calculoICMS_NF02(10000);
             Assert.AreEqual(1200, resultado);
         }
     }
